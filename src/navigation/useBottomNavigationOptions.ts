a969f47cca7ae939a8cdd919aa3cd9fc0db0f931
papercoding22 <john.ly997@gmail.com>
2024-06-02 11:32:59 +0700

    add: shadow for btm tab bar

--- src/navigation/useBottomNavigationOptions.ts
+++ src/navigation/useBottomNavigationOptions.ts
@@ -1,20 +1,30 @@
-import {ThemeContextProps} from '@/theme/ThemeProvider';
 import React from 'react';
+import {ThemeContextProps} from '@/theme/ThemeProvider';
+import {BottomTabNavigationOptions} from '@react-navigation/bottom-tabs';
 
 import BottomTabBarIcon from './BottomTabBarIcon';
 
 const useBottomNavigationOptions = (theme: ThemeContextProps) => {
-  const bottomNavigationOptions = React.useMemo(() => {
-    return {
-      headerShown: false,
-      tabBarIcon: BottomTabBarIcon,
-      tabBarStyle: {
-        backgroundColor: theme.bottomNavigationTheme.background,
-      },
-      tabBarActiveTintColor: theme.colors.onSurface,
-      tabBarInactiveTintColor: theme.colors.onSurfaceVariant,
-    };
-  }, [theme]);
+  const bottomNavigationOptions: BottomTabNavigationOptions =
+    React.useMemo(() => {
+      return {
+        headerShown: false,
+        tabBarIcon: BottomTabBarIcon,
+        tabBarStyle: {
+          backgroundColor: theme.bottomNavigationTheme.background,
+          shadowColor: theme.colors.shadow,
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: theme.elevations.level5,
+        },
+        tabBarActiveTintColor: theme.colors.onSurface,
+        tabBarInactiveTintColor: theme.colors.onSurfaceVariant,
+      };
+    }, [theme]);
 
   return bottomNavigationOptions;
 };
